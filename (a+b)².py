d2fc1dcfc244211dcb7d8849cb37d38d990f94c7
Vasanth <86598859+Vasanth1165@users.noreply.github.com>
2022-03-20 09:01:23 +0530

    Add files via upload

---
+++ (a+b)².py
@@ -0,0 +1,4 @@
+a=int(input())
+b=int(input())
+result=a**2+2*a*b+b**2
+print('result is',result)
---
+++ area of eqi tri.py
@@ -0,0 +1,6 @@
+import math as m
+a,b,c=map(int,input().split())
+x=m.sqrt(3)
+s=a**2
+z=((x/4)*s)
+print("%.2f" %z)
