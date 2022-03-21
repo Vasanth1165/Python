eac9fb8ad3296e0e1bc478f8c9439e24069f68f8
Vasanth <86598859+Vasanth1165@users.noreply.github.com>
2022-03-21 07:56:05 +0530

    Add files via upload

---
+++ week.py
@@ -0,0 +1,19 @@
+w=int(input ())
+if(w==1):
+	print('monday')
+elif(w==2):
+	print('tuesday')
+elif(w==3):
+	print('wednesday')
+elif(w==4):
+	print( 'thursday')
+elif(w==5):
+	print('friday')
+elif(w==6):
+	print('saturday')
+elif(w==7):
+	print('sunday')
+else:
+	print('give correct input')
+	
+	
