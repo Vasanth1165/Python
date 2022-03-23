4d7d322095d24eca14c1fa5b9e006c9dabbf5e7b
Vasanth <86598859+Vasanth1165@users.noreply.github.com>
2022-03-23 10:54:36 +0530

    Add files via upload

---
+++ perameters and arguments in function.py
@@ -0,0 +1,3 @@
+def location(city='ongole',country='india'):
+	print(city,'is located in',country)
+location(city='tokyo',country='japan')
---
+++ pos_sqr function.py
@@ -0,0 +1,4 @@
+def pos_sqr(a,b):
+	print(a**2+b**2+2*a*b)
+a,b=map(int,input().split())
+pos_sqr(a,b)
