f71d83eecd9124152be0256f90c6c22b1c3c8045
Vasanth <86598859+Vasanth1165@users.noreply.github.com>
2022-03-23 10:52:19 +0530

    Add files via upload

---
+++ happy numbers using function with pass keyword.py
@@ -0,0 +1,40 @@
+def is_happy(n): 
+    while n!=1 and n!=4:
+        ss = 0
+        while n:
+            r = n%10
+            ss += r*r
+            n = n//10
+
+        n = ss
+    if(n==1):
+        return True
+    else:
+        return False
+    
+
+while(True):
+    x = input('Check or Range?:')
+   
+    if(x == 'Check'):
+        num = int(input('Enter a number to check:')) 
+        if(is_happy(num)): 
+            print('Happy')
+        else:
+            print('Unhappy')
+    elif(x == 'Range'):
+        a, b = map(int, input('Enter two numbers:').split())
+        choice = input('Happy or Unhappy:')
+        if(choice == 'Happy'):
+            for i in range(a,b+1):
+                if(is_happy(i)):
+                    print(i,end = ' ')
+        else:
+            for i in range(a,b+1):
+                if(is_happy(i)):
+                	pass
+                else:
+                	print(i,end=' ')
+    else:
+        print('Invalid Input')
+        break
