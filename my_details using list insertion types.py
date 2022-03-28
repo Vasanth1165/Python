b90a135c58ee5486e5bbbc5d7abfc083c79b8afe
Vasanth <86598859+Vasanth1165@users.noreply.github.com>
2022-03-28 12:08:02 +0530

    Add files via upload

---
+++ my_details using list insertion types.py
@@ -0,0 +1,17 @@
+roll_no=264
+name="vasanth"
+course="B.TECH"
+branch="EEE"
+l=[roll_no,name,course,branch]
+l.append([70,80,75])
+l.insert(2,"Male")
+l.extend([9177522410,"vasanthyenuganti1165@gmail.com"])
+grade= sum (l[5]) / len(l[5])
+l.insert(6,grade)
+
+
+print(l)
+print(l[5])
+print(l[1][0: ])
+
+	
