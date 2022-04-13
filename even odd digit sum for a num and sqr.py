21e495bc84e26083f484f2c409eafe9e033a5a0d
Vasanth <86598859+Vasanth1165@users.noreply.github.com>
2022-04-13 19:22:01 +0530

    Add files via upload

---
+++ even odd digit sum for a num and sqr.py
@@ -0,0 +1,19 @@
+n=int(input())
+rev=0
+while(n):
+	r=n%10
+	rev=rev*10+r
+	n=n//10
+e=0
+o=0
+while(rev):
+	r=rev%10
+	rev=rev//10
+	if(r%2==0):
+		e=e*10+r
+	else:
+		o=o*10+r
+print(e)
+print(e*e)
+print(o)
+print(o*o)
